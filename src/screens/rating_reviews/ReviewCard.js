cb8a848060bda5b4ee7ec141bbf23ee40a8eea9d
Daniel <danieldawit33@gmail.com>
2021-11-20 17:20:20 -0500

    single review view

--- src/screens/rating_reviews/ReviewCard.js
+++ src/screens/rating_reviews/ReviewCard.js
@@ -3,13 +3,19 @@ import {LayoutAnimation, Text, TouchableOpacity, View} from 'react-native';
 import {colors} from "../../global/styles";
 import StarRating from "react-native-star-rating";
 import {Ionicons} from "@expo/vector-icons";
+import {CustomModal} from "../../components/CustomModal";
+import RatingsPage from "./index";
 
-export function ReviewCard({review, size, canExpand}) {
+export function ReviewCard({review, size, canExpand, defaultExpanded=false}) {
 
-    const [isExpanded, setExpanded] = useState(false);
+    const [isExpanded, setExpanded] = useState(defaultExpanded);
+    const [isModalOpen, setModalOpen] = useState(false);
 
     const toggleExpanded = () => {
-        if(!canExpand) return;
+        if(!canExpand) {
+            setModalOpen(!isModalOpen);
+            return;
+        }
         LayoutAnimation.configureNext(
             LayoutAnimation.create(
                 250,
@@ -21,67 +27,75 @@ export function ReviewCard({review, size, canExpand}) {
 
     return (
 
-        <View
-            style={{
-                backgroundColor: colors.dark_blue,
-                padding: 10,
-                width: size ? size - 10 : undefined,
-                margin: 5,
-                borderRadius: 10,
-                overflow: 'hidden',
-            }}>
+        <>
 
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <View style={{flex: 1}}>
-                    <Text
-                        style={{
-                            color: colors.aqua,
-                            fontSize: 20,
-                            fontWeight: '600',
-                        }}
-                        numberOfLines={2}
-                        adjustsFontSizeToFit={true}
-                    >
-                        {review.title}</Text>
+            <CustomModal isOpen={isModalOpen} onClose={toggleExpanded}>
+                <View style={{margin: 20}}/>
+                <RatingsPage reviewData={{reviews: [review]}} allExpanded={true} showHeader={false}/>
+            </CustomModal>
 
-                    <StarRating
-                        disabled={true}
-                        starSize={15}
-                        fullStarColor={colors.red}
-                        halfStar={colors.red}
-                        rating={review.rating}
-                        maxStars={5}
-                        halfStarEnabled={true}
-                        containerStyle={{justifyContent: undefined}}
-                        starStyle={{marginLeft: 5}}
-                    />
+            <View
+                style={{
+                    backgroundColor: colors.dark_blue,
+                    padding: 10,
+                    width: size ? size - 10 : undefined,
+                    margin: 5,
+                    borderRadius: 10,
+                    overflow: 'hidden',
+                }}>
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <View style={{flex: 1}}>
+                        <Text
+                            style={{
+                                color: colors.aqua,
+                                fontSize: 20,
+                                fontWeight: '600',
+                            }}
+                            numberOfLines={2}
+                            adjustsFontSizeToFit={true}
+                        >
+                            {review.title}</Text>
+
+                        <StarRating
+                            disabled={true}
+                            starSize={15}
+                            fullStarColor={colors.red}
+                            halfStar={colors.red}
+                            rating={review.rating}
+                            maxStars={5}
+                            halfStarEnabled={true}
+                            containerStyle={{justifyContent: undefined}}
+                            starStyle={{marginLeft: 5}}
+                        />
+                    </View>
+
+                    <View style={{alignItems: 'flex-end', flex: 1}}>
+                        <Text style={{color: colors.aqua, fontWeight: '600'}}>{review.date}</Text>
+                        <Ionicons name={'logo-apple'} color={'white'} size={20}/>
+                    </View>
+
                 </View>
 
-                <View style={{alignItems: 'flex-end', flex: 1}}>
-                    <Text style={{color: colors.aqua, fontWeight: '600'}}>{review.date}</Text>
-                    <Ionicons name={'logo-apple'} color={'white'} size={20}/>
+
+                <Text numberOfLines={canExpand ? isExpanded ? undefined : 3 : 3}
+                      ellipsizeMode={'tail'}
+                      style={{
+                          color: 'white',
+                          fontSize: 15,
+                          fontWeight: '500'
+                      }}>{review.content}</Text>
+
+                <View style={{justifyContent: 'flex-end'}}>
+                    <TouchableOpacity
+                        onPress={toggleExpanded}
+                        style={{marginTop: 5, right: 5, alignSelf: 'flex-end'}}>
+                        <Text style={{color: colors.red, fontSize: 20}}>See {isExpanded ? 'Less' : 'More'}</Text>
+                    </TouchableOpacity>
                 </View>
 
             </View>
-
-
-            <Text numberOfLines={canExpand ? isExpanded ? undefined : 3 : 3}
-                  ellipsizeMode={'tail'}
-                  style={{
-                      color: 'white',
-                      fontSize: 15,
-                      fontWeight: '500'
-                  }}>{review.content}</Text>
-
-            <View style={{justifyContent: 'flex-end'}}>
-                <TouchableOpacity
-                    onPress={toggleExpanded}
-                    style={{marginTop: 5, right: 5, alignSelf: 'flex-end'}}>
-                    <Text style={{color: colors.red, fontSize: 20}}>See {isExpanded ? 'Less' : 'More'}</Text>
-                </TouchableOpacity>
-            </View>
-
-        </View>
+        </>
 
     )
 };
--- src/screens/rating_reviews/index.js
+++ src/screens/rating_reviews/index.js
@@ -6,23 +6,32 @@ import {ReviewHeader} from "./ReviewHeader";
 import {Line} from "../../components/Line";
 import {ReviewCard} from "./ReviewCard";
 
-const renderItem = (item) => {
-    const review = item.item;
-    return <ReviewCard review={review} canExpand={true}/>
-}
 
-export default function RatingsPage({reviewData}) {
+
+export default function RatingsPage({reviewData, allExpanded, showHeader=true}) {
 
     const navigation = useNavigation();
     const reviews = reviewData ?? navigation.getState().routes[2].params.params.reviews;
 
+    const renderItem = (item) => {
+        const review = item.item;
+        return <ReviewCard review={review} canExpand={true} defaultExpanded={allExpanded}/>
+    }
+
     return (
         <View style={Styles.background}>
 
-            <View style={{width: '100%'}}>
-                <ReviewHeader reviews={reviews} hideButton={true}/>
-            </View>
-            <Line style={{marginBottom: 0}}/>
+            {showHeader ?(
+                <>
+                    <View style={{width: '100%'}}>
+                        <ReviewHeader reviews={reviews} hideButton={true}/>
+                    </View>
+                    <Line style={{marginBottom: 0}}/>
+                </>
+                )
+                : null
+
+            }
 
             <FlatList data={reviews.reviews}
                       style={{width: '100%', flex: 1}}
