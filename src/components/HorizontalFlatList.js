34f96ab67f529d4b4221e4d495fed76cc9f0d0db
Daniel <danieldawit33@gmail.com>
2021-11-19 21:21:03 -0500

    fancy horizontal scroll

--- src/components/HorizontalFlatList.js
+++ src/components/HorizontalFlatList.js
@@ -1,14 +1,20 @@
 import * as React from 'react';
-import {FlatList} from 'react-native';
+import {FlatList, Dimensions} from 'react-native';
 
-export function HorizontalFlatList({items, renderItem, numColums, listStyle, keyID}) {
+
+
+export function HorizontalFlatList({items, renderItem, numColums, listStyle, keyID, ...props}) {
+
+
     return (
             <FlatList
+                {...props}
                 key={keyID}
                 contentContainerStyle={{alignSelf: 'flex-start'}}
                 style={{borderRadius: 10, flexGrow: 0, ...listStyle}}
                 data={items}
                 numColumns={numColums}
+
                 pagingEnabled={true}
                 alwaysBounceHorizontal={false}
                 alwaysBounceVertical={false}
--- src/components/LargeAppDisplay.js
+++ src/components/LargeAppDisplay.js
@@ -1,28 +1,33 @@
 import * as React from 'react';
-import {View, Image, Dimensions, Text} from 'react-native';
+import {Dimensions, Image, Text, View} from 'react-native';
 import {HorizontalFlatList} from "./HorizontalFlatList";
+
+const {width} = Dimensions.get('window');
+const previewCount = 1;
+const itemWidth = width/(previewCount+.5);
+const startScroll = (itemWidth*3/4);
 
 const renderApp = (appList) => {
     const item = appList.item.results[2]
     const url = item.artworkUrl512;
 
-    return(
-        <>
-            <View style={{width: Dimensions.get('window').width-40, alignSelf: 'center'}}>
-                <Text style={{
-                    fontSize: 30,
-                    color: 'white'
-                }}>{appList.item.title}</Text>
-                <Image source={{uri: url}}
-                       style={{
-                           width: Dimensions.get('window').width-40,
-                           height: 250,
-                           resizeMode: 'cover',
-                           borderRadius: 20,
-                           alignSelf: 'center'
-                       }}/>
-            </View>
-        </>
+    return (
+
+        <View style={{ margin: 5, alignSelf: 'center', width: itemWidth-10}}>
+            <Text style={{
+                fontSize: 30,
+                color: 'white'
+            }}>{appList.item.title}</Text>
+            <Image source={{uri: url}}
+                   style={{
+                       width: itemWidth - 10,
+                       height: 250,
+                       resizeMode: 'cover',
+                       borderRadius: 20,
+                       alignSelf: 'center'
+                   }}/>
+        </View>
+
 
     );
 
@@ -30,11 +35,20 @@ const renderApp = (appList) => {
 
 export function LargeAppDisplay({apps}) {
 
+    const snapToOffset = apps.map((_, i) => {
+        return ((i * itemWidth) + startScroll)
+    })
+
+
     return (
-            <HorizontalFlatList items={apps}
-                                key={apps.length}
-                                numColums={apps.length}
-                                renderItem={renderApp}
-            />
+        <HorizontalFlatList items={apps}
+                            decelerationRate={0}
+                            horizontal={true}
+                            snapToOffsets={snapToOffset}
+                            snapToAlignment={'center'}
+                            key={apps.length}
+                            numColums={undefined}
+                            renderItem={renderApp}
+        />
     );
 };
