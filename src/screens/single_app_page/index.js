1f470cc386da3262327fe7aa8eb7d7b55bee6152
Daniel <danieldawit33@gmail.com>
2021-11-20 10:54:56 -0500

    change color pallet

--- src/screens/single_app_page/index.js
+++ src/screens/single_app_page/index.js
@@ -1,38 +1,49 @@
 import * as React from 'react';
-import {Image, SafeAreaView, Text, View} from 'react-native';
+import {Image, SafeAreaView, Text, View, ScrollView} from 'react-native';
 import {colors, Styles} from '../../global/styles'
 import PropTypes from 'prop-types'
 import {useNavigation} from "@react-navigation/native";
+import {AppTitleHeading} from "./AppTitleHeading";
+import StarRating from 'react-native-star-rating';
 
 export default function SingleApp({app}) {
 
     const navigation = useNavigation();
     const appData = app ?? navigation.getState().routes[1].params.params.app;
     const image = appData.artworkUrl512
+    console.log(appData.userRatingCount.toSymbolic(), appData.userRatingCount)
 
     return (
-        <SafeAreaView style={Styles.background}>
-            <View style={{flexDirection: 'row', flex: 1, padding: 10}}>
-                <Image source={{uri: image}} style={{width: 150, height: 150, borderRadius: 30}}/>
-                <View style={{marginLeft: 10, padding: 10, flex: 1}}>
+        <ScrollView style={[Styles.background, {alignItems: undefined}]}
+                    contentContainerStyle={{alignItems: 'center'}}>
+
+                <AppTitleHeading subtitle={appData.subtitle}
+                                 name={appData.trackCensoredName}
+                                 imageURL={image}
+                />
+
+            <ScrollView horizontal={true}>
+                <View style={{alignItems: 'center'}}>
+
                     <Text style={{
-                        color: 'white',
-                        fontSize: 30,
-                        fontWeight: '600'
-                    }}
-                          adjustsFontSizeToFit={true}
-                          numberOfLines={2}
-                    >{appData.trackCensoredName}</Text>
-                    <Text style={{
-                        color: colors.aqua,
-                        fontSize: 20
-                    }}
-                          adjustsFontSizeToFit={true}
-                          numberOfLines={2}
-                    >{appData.subtitle}</Text>
+                        fontSize: 20,
+                        color: colors.pink
+                    }}>{appData.userRatingCount.toSymbolic()}</Text>
+
+                    <StarRating
+                        disabled={true}
+                        maxStars={5}
+                        rating={appData.averageUserRating}
+                        fullStarColor={colors.red}
+                        halfStarColor={colors.red}
+                        starSize={20}
+
+                    />
                 </View>
-            </View>
-        </SafeAreaView>
+            </ScrollView>
+
+
+        </ScrollView>
     );
 };
 
