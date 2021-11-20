c35f05d6f6cf3cca54b5f4aeec018f90b7b3835d
Daniel <danieldawit33@gmail.com>
2021-11-19 21:35:10 -0500

    add single app page

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,1 +1,2 @@
 export {default as Apps} from './apps_page/index';
+export {default as SingleApp} from './single_app_page/index';
---
+++ src/screens/single_app_page/index.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import PropTypes from 'prop-types'
+
+export function SingleApp({app}) {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+
+SingleApp.propTypes = {
+    app: PropTypes.object,
+}
