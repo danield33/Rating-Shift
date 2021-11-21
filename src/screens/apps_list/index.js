965e16a824f64889a976dfafce3785cbb56e7cf9
Daniel <danieldawit33@gmail.com>
2021-11-21 01:12:37 -0500

    Update index.js

--- src/screens/apps_list/index.js
+++ src/screens/apps_list/index.js
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import {View, FlatList} from 'react-native';
 import {Styles} from "../../global";
-import {useNavigation} from "@react-navigation/native";
 import {useSelector} from "react-redux";
+import {SideAppSelector} from "../../components/SideAppSelector";
 
 const renderApp = (appItem) => {
-    return(
-        <View></View>
-    )
+    const app = appItem.item;
+    return <SideAppSelector app={app}/>
 }
 
 export default function AppsList() {
 
     const apps = useSelector(state => state.currentApps);
-
-    console.log(apps, 1)
 
     return (
         <View style={Styles.background}>
