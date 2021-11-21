87b0bef233985237c45df2fd160c739973947ca3
Daniel <danieldawit33@gmail.com>
2021-11-21 11:13:37 -0500

    Add category selection

--- src/screens/apps_page/LargeCategoryImage.js
+++ src/screens/apps_page/LargeCategoryImage.js
@@ -1,26 +1,38 @@
 import * as React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, View, TouchableWithoutFeedback} from 'react-native';
+import {useDispatch} from "react-redux";
+import {useNavigation} from "@react-navigation/native";
+import {setApps} from "../../global/redux/actions/AppListActions";
 
 export function LargeCategoryImage({appList, width}) {
     const item = appList.item.results[0];
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
     const url = item.artworkUrl512;
+
+    const onPress = () => {
+        dispatch(setApps(appList.item.results));
+        navigation.navigate('AppsList')
+    }
 
     return (
 
-        <View style={{margin: 5, alignSelf: 'center', width: width - 10}}>
-            <Text style={{
-                fontSize: 30,
-                color: 'white'
-            }}>{appList.item.title}</Text>
-            <Image source={{uri: url}}
-                   style={{
-                       width: width - 10,
-                       height: 250,
-                       resizeMode: 'cover',
-                       borderRadius: 20,
-                       alignSelf: 'center'
-                   }}/>
-        </View>
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View style={{margin: 5, alignSelf: 'center', width: width - 10}}>
+                <Text style={{
+                    fontSize: 30,
+                    color: 'white'
+                }}>{appList.item.title}</Text>
+                <Image source={{uri: url}}
+                       style={{
+                           width: width - 10,
+                           height: 250,
+                           resizeMode: 'cover',
+                           borderRadius: 20,
+                           alignSelf: 'center'
+                       }}/>
+            </View>
+        </TouchableWithoutFeedback>
 
 
     );
