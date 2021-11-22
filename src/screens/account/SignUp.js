0ec68736c05dcbad2e3abddc80d6298a7b524ad9
Daniel <danieldawit33@gmail.com>
2021-11-22 16:45:36 -0500

    yup schema

--- src/screens/account/SignUp.js
+++ src/screens/account/SignUp.js
@@ -1,18 +1,57 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Image, TextInput, View, StyleSheet, Text} from 'react-native';
 import {Ionicons} from "@expo/vector-icons";
 import colors from "../../global/styles/colors";
 import {If} from "../../components/If";
 import {TextInputValue} from "../../components/TextInputValue";
+import * as yup from 'yup';
+import {FlatButton} from "../../components/FlatButton";
+
+const yupSchema = yup.object().shape({
+    username: yup.string().required("Please input a username").min(3),
+    password: yup.string().required("Please input a password").min(6),
+});
 
 export function SignUp({confirmPassword, onSubmit}) {
+
+    const [errMessages, setErrMessages] = useState({
+        username: null,
+        password: null,
+        confirmPassword: null
+    })
+
+    const input = {
+        username: '',
+        password: '',
+        // confirmPass: ''
+    }
+    let confirmPassSchema;
+
+    useEffect(() => {
+        if(confirmPassword)
+            confirmPassSchema = yup.string().required("Please confirm your password").min(6)
+    }, [])
+
+    const textChange = (inputKey) => {
+        return (text) => {
+            input[inputKey] = text;
+            confirmPassSchema?.validate(input.confirmPass).catch(err => {
+                errMessages.confirmPassword = err.message
+                setErrMessages(errMessages)
+            })
+            yupSchema.validate(input)
+                .catch(err => {
+                errMessages[err.path] = err.message
+                setErrMessages(errMessages)
+            })
+        }
+    }
+
 
     const [pfp, setPfp] = useState(null);
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%'}}>
-
-
 
             <If can={!confirmPassword}>
                 <Text style={styles.titleText}>Log In</Text>
@@ -26,27 +65,43 @@ export function SignUp({confirmPassword, onSubmit}) {
 
             <TextInputValue
                 style={styles.textInput}
+                blur={true}
+                endEditing={true}
+                onSetText={textChange('username')}
                 placeholder={'Username'}
                 autoCorrect={false}
                 autoCapitalize={'none'}
             />
+            {errMessages.username && <Text>{errMessages.username}</Text>}
             <TextInputValue
                 style={styles.textInput}
+                blur={true}
+                endEditing={true}
+                onSetText={textChange('password')}
                 autoCorrect={false}
                 autoCapitalize={'none'}
                 placeholder={'Password'}
                 secureTextEntry
             />
+            {errMessages.password && <Text>{errMessages.password}</Text>}
+
             <If can={confirmPassword}>
-                <TextInputValue
-                    style={styles.textInput}
-                    autoCorrect={false}
-                    autoCapitalize={'none'}
-                    placeholder={'Confirm Password'}
-                    secureTextEntry
-                />
+                <>
+                    <TextInputValue
+                        blur={true}
+                        endEditing={true}
+                        style={styles.textInput}
+                        onSetText={textChange('confirmPass')}
+                        autoCorrect={false}
+                        autoCapitalize={'none'}
+                        placeholder={'Confirm Password'}
+                        secureTextEntry
+                    />
+                    {errMessages.confirmPassword && <Text>{errMessages.confirmPassword}</Text>}
+                </>
             </If>
 
+            <FlatButton text={'Submit'} color={colors.red} style={{width: '100%', top: 30}}/>
 
         </View>
     );
