323b1722d4f594761327a8650246492ce5e2b0a0
Daniel <danieldawit33@gmail.com>
2021-11-22 16:05:22 -0500

    add text input value

---
+++ src/components/TextInputValue.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {TextInput} from 'react-native';
+
+export function TextInputValue({...props, endEditing, submitEditing, blur, textChange, onTextChange, onSetText}) {
+
+    let inputText = "";
+
+    const onChange = (text) => {
+        inputText = text;
+        onTextChange(inputText);
+    }
+
+    const setText = (event) => {
+        if(event.nativeEvent.text !== undefined){
+            inputText = event.nativeEvent.text;
+            onSetText(inputText);
+        }
+    }
+
+    return (
+        <TextInput {...props}
+                   onChangeText={textChange && onChange}
+                   onEndEditing={endEditing && setText}
+                   onSubmitEditing={submitEditing && setText}
+                   onBlur={blur && setText}/>
+    );
+};
