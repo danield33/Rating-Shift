1a87499c468cc7b81bc92fc9b3693b25fb1b3729
Daniel <danieldawit33@gmail.com>
2021-11-22 15:27:43 -0500

    add If Component

---
+++ src/components/If.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {View} from 'react-native';
+
+export function If({can}) {
+    const childArr = React.Children.toArray(children);
+    if(can && childArr.length > 0)
+        return childArr[0];
+    else if(childArr.length > 1) return childArr[1];
+    else return null;
+
+};
--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 import {View} from 'react-native';
 import {Styles} from "../../global";
 
+const isLoggedIn = false;
+
 export default function Account() {
     return (
         <View style={Styles.background}>
+
+
 
         </View>
     );
