39d0f5c79547436808280c2acc601ab921ed9e6b
Daniel <danieldawit33@gmail.com>
2021-11-22 19:55:26 -0500

    Update Permissions.js

--- src/global/util/Permissions.js
+++ src/global/util/Permissions.js
@@ -1,17 +1,12 @@
-import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
-import * as Permissions from "expo-camera";
 
 export const checkForCameraRollPermission = async () =>{
-    const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
 
     const {status: existingStatus} = await MediaLibrary.getPermissionsAsync()
     let finalStatus = existingStatus;
-    // If we don't already have permission, ask for it
     if (existingStatus !== 'granted') {
         const {status} = await MediaLibrary.requestPermissionsAsync()
         finalStatus = status;
     }
-    console.log(finalStatus, 'fstatus')
-    return status === 'granted';
+    return finalStatus === 'granted';
 }
