fb110a2629ed01bad4018dd91d292029650df06e
Daniel <danieldawit33@gmail.com>
2021-11-22 19:00:05 -0500

    error handling

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,30 @@
 import FTMatters from "./42Matters";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 class RShift {
 
     ftMatters = new FTMatters();
 
+    createAccount({username, email, password}){
+        const auth = getAuth();
+        return new Promise((resolve, reject) => {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    resolve(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    reject([errorCode, errorMessage])
+                });
+        })
+    }
+
 }
 
 export default new RShift();
+
+export const errorCodes = {
+    "auth/email-already-in-use": "Email is already in use! Please try again"
+}
--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {Alert, Text, View} from 'react-native';
 import {Styles} from "../../global";
 import {If} from "../../components/If";
 import colors from "../../global/styles/colors";
 import {FlatButton} from "../../components/FlatButton";
 import {CustomModal} from "../../components/CustomModal";
 import {SignUp} from "./SignUp";
+import RShift, {errorCodes} from '../../database'
 
 const isLoggedIn = false;
 
@@ -13,11 +14,23 @@ export default function Account() {
 
     const [isSigningUp, setSigningUp] = useState(0);//0 = nothing 1 = log in 2 = create account
 
+    const createAccount = (data) => {
+        RShift.createAccount(data).then(user => {
+            console.log(user, 'us')
+        })
+            .catch((err) => {
+                const errorCode = errorCodes[err[0]];
+                if (errorCode)
+                    Alert.alert(errorCode);
+                else Alert.alert(err[0]);
+            })
+    }
+
     return (
         <View style={Styles.background}>
 
             <CustomModal isOpen={Boolean(isSigningUp)} onClose={() => setSigningUp(0)}>
-                <SignUp confirmPassword={isSigningUp-1} onSubmit={console.log}/>
+                <SignUp confirmPassword={isSigningUp - 1} onSubmit={createAccount}/>
             </CustomModal>
 
             <If can={!isLoggedIn}>
@@ -29,8 +42,10 @@ export default function Account() {
                         fontWeight: '500'
                     }}>Select an Option</Text>
 
-                    <FlatButton color={colors.red} text={"Log In"} style={{width: '100%'}} onPress={() => setSigningUp(1)}/>
-                    <FlatButton color={colors.dark_blue} text={"Create Account"} style={{width: '100%'}} onPress={() => setSigningUp(2)}/>
+                    <FlatButton color={colors.red} text={"Log In"} style={{width: '100%'}}
+                                onPress={() => setSigningUp(1)}/>
+                    <FlatButton color={colors.dark_blue} text={"Create Account"} style={{width: '100%'}}
+                                onPress={() => setSigningUp(2)}/>
                 </View>
 
             </If>
