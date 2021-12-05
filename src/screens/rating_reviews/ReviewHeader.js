20483f2d8f9294c445a966ce61ea3524a2cdd1ab
Daniel <danieldawit33@gmail.com>
2021-12-04 20:20:51 -0500

    add writing review component

--- src/screens/rating_reviews/ReviewHeader.js
+++ src/screens/rating_reviews/ReviewHeader.js
@@ -1,16 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, TouchableOpacity} from 'react-native';
 import colors from "../../global/styles/colors";
 import StarRating from "react-native-star-rating";
 import {useNavigation} from "@react-navigation/native";
 import {useDispatch, useSelector} from "react-redux";
 import RShift from '../../database'
+import {Ionicons} from "@expo/vector-icons";
+import {CustomModal} from "../../components/CustomModal";
+import {WriteReview} from "./WriteReview";
 
 export function ReviewHeader({hideButton = false}) {
     const navigation = useNavigation();
     const trackId = useSelector(state => state.appList.currentlyViewing.item);
     const [rating, setRating] = useState(0);
     const [app, setApp] = useState(null);
+    const [writeReview, setWriting] = useState(false);
 
     useEffect(() => {
         const aborter = RShift.apps.get(trackId, app => {
@@ -26,6 +30,11 @@ export function ReviewHeader({hideButton = false}) {
 
     return (
         <View>
+
+            <CustomModal isOpen={writeReview} onClose={() => setWriting(false)}>
+                <WriteReview/>
+            </CustomModal>
+
             <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
                 <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>Reviews</Text>
                 {hideButton ? null :
@@ -71,6 +80,15 @@ export function ReviewHeader({hideButton = false}) {
                     selectedStar={setRating}
                 />
             </View>
+
+            <TouchableOpacity style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }} onPress={() => setWriting(true)}>
+                <Ionicons name={'clipboard'} color={colors.aqua} size={25}/>
+                <Text style={{fontSize: 20, padding: 5, color: colors.aqua, fontWeight: '600'}}>Write a Review</Text>
+            </TouchableOpacity>
         </View>
     );
 }
---
+++ src/screens/rating_reviews/WriteReview.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View} from 'react-native';
+
+export function WriteReview({onSubmit}) {
+    return (
+        <View>
+
+        </View>
+    );
+};
